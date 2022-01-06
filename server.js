a6d6cfc45c15c6cc45facb31e445996070d18722
Jonatan <johntechs.dev@gmail.com>
2022-01-06 10:10:42 +0000

    Added feature to upload a file and get metadata as JSON

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
 require('dotenv').config()
 
-var app = express();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.use((req, _, next) => {
+  console.log(req.method, req.path, req.params, req.body);
+  next();
+});
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/')
+  }
+});
+
+const upload = multer({ storage: storage })
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file
+  res.json({
+    name: originalname,
+    type: mimetype, 
+    size
+  });
 });
 
 
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
